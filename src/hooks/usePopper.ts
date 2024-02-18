7bc7f2a7af79bc6394ea09665b57247abc85877e
stefanBid <biddau.stefano99@gmail.com>
2024-02-18 15:46:07 +0100

    feat: add project hooks

---
+++ src/hooks/usePopper.ts
@@ -0,0 +1,73 @@
+//usePopper.ts
+
+import { ref } from 'vue';
+import {
+  useFloating,
+  flip,
+  shift,
+  autoUpdate,
+  offset,
+  arrow,
+} from '@floating-ui/vue';
+import type { Placement } from '@floating-ui/dom';
+
+type PopperType = 'dropdownMenu' | 'helpInfo' | 'regexEditor' | 'combobox';
+
+// This helper function is used to configure the popper settings for each popper type in complete security
+const configurePopperSetting = (
+  placement: Placement,
+  offsetValue: number,
+  hasArrow: boolean,
+) => ({
+  placement,
+  offsetValue,
+  hasArrow,
+});
+
+const popperSettings = {
+  dropdownMenu: configurePopperSetting('bottom-end', 4, false),
+  helpInfo: configurePopperSetting('top', 10, true),
+  regexEditor: configurePopperSetting('bottom', 4, false),
+  combobox: configurePopperSetting('bottom', 4, false),
+};
+
+export function usePopper(popperType: PopperType) {
+  const anchor = ref();
+  const popper = ref();
+  const popperArrow = ref();
+  const isOpen = ref();
+
+  const { floatingStyles, placement } = useFloating(anchor, popper, {
+    open: isOpen,
+    placement: popperSettings[popperType].placement,
+    strategy: 'absolute',
+    transform: false,
+    middleware: [
+      offset(popperSettings[popperType].offsetValue),
+      flip(),
+      shift(),
+      ...(popperSettings[popperType].hasArrow
+        ? [arrow({ element: popperArrow })]
+        : []),
+    ],
+    whileElementsMounted: autoUpdate,
+  });
+
+  const changeToolTipVisibility = (state: 'open' | 'close') => {
+    if (state === 'open') {
+      isOpen.value = true;
+    } else {
+      isOpen.value = false;
+    }
+  };
+
+  return {
+    anchor,
+    popper,
+    popperArrow,
+    isOpen,
+    popperPosition: placement,
+    popperStyle: floatingStyles,
+    changeToolTipVisibility,
+  };
+}
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,12 @@
+// useTheme.ts
+import { useDark, useToggle } from '@vueuse/core';
+
+export function useTheme() {
+  const isDark = useDark();
+  const toggleTheme = useToggle(isDark);
+
+  return {
+    isDark,
+    toggleTheme,
+  };
+}
