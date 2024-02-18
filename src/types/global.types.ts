a678d485e07a1c0115771e474251865fc0a496b8
stefanBid <biddau.stefano99@gmail.com>
2024-02-18 16:00:03 +0100

    feat:add global types

---
+++ src/types/global.types.ts
@@ -0,0 +1,9 @@
+export type ColorVariant =
+  | 'blue'
+  | 'green'
+  | 'red'
+  | 'yellow'
+  | 'gray'
+  | 'violet';
+
+export type SizeVariant = 'small' | 'base' | 'large';
--- src/utils/idsGeneration.utils.ts
+++ src/utils/idsGeneration.utils.ts
@@ -1,5 +1,4 @@
 // idsGeneration.utils.ts
-
 import { v4 as uuidv4 } from 'uuid';
 export const generateId = (): string => {
   return uuidv4();
